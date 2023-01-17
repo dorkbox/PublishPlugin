39a8f75c493b95f1afc4375dbca005a061385c9c
Robinson <robinson@dorkbox.com>
2023-01-17 01:50:53 +0100

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ object Extras {
     // set for the project
     const val description = "Gradle Plugin to publish projects to the sonatype repository"
     const val group = "com.dorkbox"
-    const val version = "1.15"
+    const val version = "1.16"
 
     // set as project.ext
     const val name = "Gradle Publish"
